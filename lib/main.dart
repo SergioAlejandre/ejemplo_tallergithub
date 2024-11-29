b977cf958e54a311ccd0ced8a5e8051dd5e0986f
SergioAlejandre <a2223330133@alumnos.uat.edu.mx>
2024-11-29 16:31:38 -0600

    cambiar_titulo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Taller de Github en flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
